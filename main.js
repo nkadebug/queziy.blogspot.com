ed75a2deb6e1ec8ed0034b1c463f06a372b3a49e
nkadebug <66764893+nkadebug@users.noreply.github.com>
2020-09-19 16:23:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -42,11 +42,11 @@ function getHtml(url,cb) {
     console.log("getHtml", "Fetching...", url);
     fetch(url).then(res=>res.text()).then(function (resp) {
       htmlCache[url] = $(resp).filter("#main").html();
-	  if(typeOf(cb) == "function")cb(htmlCache[url]);
+	  if(typeof(cb) == "function")cb(htmlCache[url]);
     });
   } else {
     console.log("getHtml", "Cached", url);
-    if(typeOf(cb) == "function")cb(htmlCache[url]);
+    if(typeof(cb) == "function")cb(htmlCache[url]);
   }
 }
 
